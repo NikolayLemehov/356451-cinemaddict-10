ea31afbe23c0f87380b9940e242162f7898f59bd
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-02-02 07:16:09 +0200

    Исправляет ошибку main-navigation-controller.js комитя не используемый код

--- src/controllers/main-navigation-controller.js
+++ src/controllers/main-navigation-controller.js
@@ -9,19 +9,16 @@ export default class MainNavigationController {
 
     this._mainNavigationComponent = null;
 
-    this._onDataChange = this._onDataChange.bind(this);
-    this._onMenuItemChange = this._onMenuItemChange.bind(this);
-
-    // this._filmsModel.setDataChangeHandler(this._onDataChange);
+    // this._onDataChange = this._onDataChange.bind(this);
+    // this._onMenuItemChange = this._onMenuItemChange.bind(this);
   }
 
   render() {
-    // const menuItems = this._filmsModel.getMenuItems();
     const menuItems = mainNav;
     const oldComponent = this._mainNavigationComponent;
 
     this._mainNavigationComponent = new MainNavigationComponent(menuItems);
-    this._mainNavigationComponent.setMenuItemChangeHandler(this._onMenuItemChange);
+    // this._mainNavigationComponent.setMenuItemChangeHandler(this._onMenuItemChange);
     if (oldComponent) {
       replaceElement(this._mainNavigationComponent, oldComponent);
     } else {
@@ -29,11 +26,11 @@ export default class MainNavigationController {
     }
   }
 
-  _onMenuItemChange(menuItemName) {
-    this._filmsModel.setMenuItemName(menuItemName);
-  }
+  // _onMenuItemChange(menuItemName) {
+  //   this._filmsModel.setMenuItemName(menuItemName);
+  // }
 
-  _onDataChange() {
-    this.render();
-  }
+  // _onDataChange() {
+  //   this.render();
+  // }
 }
